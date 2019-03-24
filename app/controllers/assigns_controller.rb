284e70dddaa2f6efa58f6f6c598dd58f495fed96
koda <hogehogehoge@gmail.com>
2019-03-24 16:44:12 +0900

    招待ユーザーの重複を避けるcontroller追記

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -4,9 +4,13 @@ class AssignsController < ApplicationController
   def create
     team = Team.friendly.find(params[:team_id])
     user = email_reliable?(assign_params) ? User.find_or_create_by_email(assign_params) : nil
-    if user
+    user_id = User.where(email: user.email).select(:id)
+    count = Assign.where(team_id: team.id).where(user_id: user_id).count
+    if user && count == 0
       team.invite_member(user)
       redirect_to team_url(team), notice: 'アサインしました！'
+    elsif user  && count >= 1
+      redirect_to team_url(team), notice: 'すでに登録されています'
     else
       redirect_to team_url(team), notice: 'アサインに失敗しました！'
     end
@@ -16,7 +20,6 @@ class AssignsController < ApplicationController
     assign = Assign.find(params[:id])
     if assign.user_id == current_user.id || current_user == assign.team.owner
       destroy_message = assign_destroy(assign, assign.user)
-
       redirect_to team_url(params[:team_id]), notice: destroy_message
     else
       redirect_to teams_url
